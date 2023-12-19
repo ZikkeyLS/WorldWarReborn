58bf2071e9d62dd43e766b7d248d1a2feba246be
ZikkeyLS <hisaha21@yandex.ru>
2023-12-20 00:41:24 +0300

    Initialize CMake

---
+++ WorldWarReborn.cpp
@@ -0,0 +1,12 @@
+﻿// WorldWarReborn.cpp : Defines the entry point for the application.
+//
+
+#include "WorldWarReborn.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
